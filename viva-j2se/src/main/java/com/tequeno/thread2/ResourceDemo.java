38ead2ce4b3b1cbaaf6b6f3e72ae4322b3e2bcf6
pixingdaiyue051 <pixingdaiyue051@163.com>
2019-02-27 09:50:20 +0800

    修改为循环开启线程

--- viva-j2se/src/main/java/com/tequeno/thread2/ResourceDemo.java
+++ viva-j2se/src/main/java/com/tequeno/thread2/ResourceDemo.java
@@ -7,19 +7,13 @@ public class ResourceDemo {
 		ResourceProducer rr = new ResourceProducer(r);
 		ResourceConsumer rc = new ResourceConsumer(r);
 
-		Thread t1 = new Thread(rr);
-		t1.start();
-		Thread t2 = new Thread(rr);
-		t2.start();
-		Thread t3 = new Thread(rr);
-		t3.start();
-		
-		Thread t4 = new Thread(rc);
-		t4.start();
-		Thread t5 = new Thread(rc);
-		t5.start();
-		Thread t6 = new Thread(rc);
-		t6.start();
+		for (int i = 0; i < 3; i++) {
+			new Thread(rr).start();
+		}
+
+		for (int i = 0; i < 5; i++) {
+			new Thread(rc).start();
+		}
 	}
 
 }
