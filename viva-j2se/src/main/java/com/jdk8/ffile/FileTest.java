206ad3e001bf239dfc6679dc4eac21b6132f575c
hexk <tequenohe@shinyway.com.cn>
2020-12-25 13:00:20 +0800

    nio-channel,buffer

--- viva-j2se/src/main/java/com/jdk8/ffile/FileTest.java
+++ viva-j2se/src/main/java/com/jdk8/ffile/FileTest.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Optional;
@@ -12,30 +13,39 @@ import java.util.stream.Stream;
 
 public class FileTest {
     public static void main(String[] args) {
+//        File file = new File("D:\\hexk\\测试数据文档\\全员类.pdf");
+//        String name = file.getName();
+//        System.out.println(name);
+//        long freeSpace = file.getFreeSpace();
+//        System.out.println(freeSpace);
+//        long length = file.length();
+//        System.out.println(length);
 
-        // nio生产文件流
-        long uniqueWords = 0;
-        try (Stream<String> lines =
-                     Files.lines(Paths.get(".", "doc/data.txt").normalize(), Charset.defaultCharset())) {
-            uniqueWords = lines.flatMap(line -> Arrays.stream(line.split("")))
-//                    .distinct()
-//                    .filter(l -> !l.matches(" "))
-                    .count();// 统计文件字符数
-//            uniqueWords = lines.count();// 行数
-            System.out.println(uniqueWords);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        recursiveListFile(Paths.get(".", "doc/data.txt").normalize().toString());
-        recursiveListFile(Paths.get("../", "viva-la-vida").normalize().toString());
+//        // nio生产文件流
+//        long uniqueWords = 0;
+//        try (Stream<String> lines =
+//                     Files.lines(Paths.get("doc/data.txt").normalize(), Charset.defaultCharset())) {
+//            uniqueWords = lines.flatMap(line -> Arrays.stream(line.split("")))
+////                    .distinct()
+////                    .filter(l -> !l.matches(" "))
+//                    .count();// 统计文件字符数
+////            uniqueWords = lines.count();// 行数
+//            System.out.println(uniqueWords);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        FileTest fileTest = new FileTest();
+//        fileTest.recursiveListFile(Paths.get("doc").normalize().toString());
+//        fileTest.recursiveListFile(Paths.get("../viva-la-vida").normalize().toString());
+
     }
 
-    public static void recursiveListFile(String filePath) {
+    private void recursiveListFile(String filePath) {
         File f = new File(filePath);
         recursiveListFile(f, 0);
     }
 
-    private static void recursiveListFile(File file, int len) {
+    private void recursiveListFile(File file, int len) {
         IntStream.range(0, len).forEach(i -> System.out.print("\t"));
         System.out.println(file.getName());
         if (file.isDirectory()) {
---
+++ viva-j2se/src/main/java/com/jdk8/ffile/NioFileTest.java
@@ -0,0 +1,83 @@
+package com.jdk8.ffile;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+/**
+ * @Desription:
+ * @Author: hexk
+ */
+public class NioFileTest {
+
+    public static void main(String[] args) {
+//        RandomAccessFile file =  new RandomAccessFile("");
+//        file.getChannel()
+        NioFileTest nioFileTest = new NioFileTest();
+//            nioFileTest.testBuffer();
+        nioFileTest.testChannel();
+    }
+
+    private void testBuffer() {
+        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+        // 看一下初始时4个核心变量的值
+        readInfo(byteBuffer);
+        // 添加一些数据到缓冲区中
+        String s = "aka";
+        byteBuffer.put(s.getBytes());
+        // 看一下初始时4个核心变量的值
+        readInfo(byteBuffer);
+        // 切换读模式
+        byteBuffer.flip();
+        readInfo(byteBuffer);
+        // 创建一个limit()大小的字节数组(因为就只有limit这么多个数据可读)
+        byte[] bytes = new byte[byteBuffer.limit()];
+        // 将读取的数据装进我们的字节数组中
+        byteBuffer.get(bytes);
+        // 输出数据
+        System.out.println(new String(bytes, 0, bytes.length));
+        readInfo(byteBuffer);
+        // 清空缓冲区,回到写模式
+        byteBuffer.clear();
+        readInfo(byteBuffer);
+    }
+
+    private void readInfo(ByteBuffer byteBuffer) {
+        System.out.println("limit--->" + byteBuffer.limit());
+        System.out.println("position--->" + byteBuffer.position());
+        System.out.println("capacity--->" + byteBuffer.capacity());
+        System.out.println("mark--->" + byteBuffer.mark());
+        System.out.println("--------------------------------------");
+    }
+
+    private void testChannel() {
+        try (FileChannel inChannel = FileChannel.open(Paths.get("doc/data.txt"), StandardOpenOption.READ);
+             FileChannel outChannel = FileChannel.open(Paths.get("D:\\hexk\\测试数据文档\\data1.txt"), StandardOpenOption.APPEND, StandardOpenOption.CREATE)) {
+            // channel配合buffer完成文件读写
+//            ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+//            while (inChannel.read(byteBuffer) != -1) {
+//                byteBuffer.flip();
+//                outChannel.write(byteBuffer);
+//                byteBuffer.clear();
+//            }
+
+            // 直接使用transfer实现通道之间数据传输
+//            inChannel.transferTo(0, inChannel.size(), outChannel);
+//            outChannel.transferFrom(inChannel, 0, inChannel.size());
+
+            // channel配合MappedByteBuffer完成文件读写,直接操作缓冲区
+//            MappedByteBuffer inMapper = inChannel.map(FileChannel.MapMode.READ_ONLY, 0, inChannel.size());
+//            MappedByteBuffer outMapper = outChannel.map(FileChannel.MapMode.READ_WRITE, 0, inChannel.size());
+//            byte[] b = new byte[inMapper.limit()];
+//            inMapper.get(b);
+//            outMapper.put(b);
+//            String s = new String(b);
+//            System.out.println(s);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
