8af07cbdc20a694b27829446076dcc273522a127
hexk <hexiaokun@hzjiuguan.net>
2022-06-09 14:23:27 +0800

    不引用最新jar版本

--- viva-j2se/src/main/java/com/tequeno/json/JsonTest.java
+++ viva-j2se/src/main/java/com/tequeno/json/JsonTest.java
@@ -1,6 +1,6 @@
 package com.tequeno.json;
 
-import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson.JSON;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
