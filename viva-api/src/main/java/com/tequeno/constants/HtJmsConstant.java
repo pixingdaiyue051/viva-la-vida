f36cd44485b835f4a274c9f71df020d6a3fed84e
hexk <hexk@111.com>
2024-09-03 16:12:00 +0800

    rabbitmq

--- viva-api/src/main/java/com/tequeno/constants/HtJmsConstant.java
+++ viva-api/src/main/java/com/tequeno/constants/HtJmsConstant.java
@@ -2,6 +2,7 @@ package com.tequeno.constants;
 
 public class HtJmsConstant {
 
+    //// rocketmq
     public static final String ROCKET_R_TOPIC = "r_topic";
 
     /**
@@ -13,4 +14,36 @@ public class HtJmsConstant {
     public static final String ROCKET_TAG_A = "r_tag_a";
 
     public static final String ROCKET_TAG_B = "r_tag_b";
+
+    //// rabbitmq
+    public static final String EXCHANGE_NAME = "r_exchange";
+
+    public static final String EXCHANGE_TYPE = "direct";
+
+    public static final String QUEUE_NAME = "r_queue";
+
+    public static final String ROUTING_KEY = "r_routing";
+
+    //// activemq
+    /**
+     * jms主题订阅模式容器bean名字
+     */
+    public final static String TOPIC_CONTAINER_FACTORY = "jmsListenerContainerTopic";
+
+    /**
+     * jms队列模式容器bean名字
+     */
+    public final static String QUEUE_CONTAINER_FACTORY = "jmsListenerContainerQueue";
+
+    /**
+     * 队列名字
+     */
+    public final static String QUEUE_SCHEDULED_NAME = "scheduledQueue";
+    public final static String QUEUE_SIMPLE_NAME = "simpleQueue";
+
+    /**
+     * 主题名字
+     */
+    public final static String TOPIC_SCHEDULED_NAME = "scheduledTopic";
+    public final static String TOPIC_SIMPLE_NAME = "simpleTopic";
 }
--- viva-api/src/main/java/com/tequeno/constants/HtPropertyConstant.java
+++ viva-api/src/main/java/com/tequeno/constants/HtPropertyConstant.java
@@ -83,34 +83,4 @@ public class HtPropertyConstant {
     public final static String REG_MAIL = "^[a-z0-9A-Z]+[- | a-z0-9A-Z . _]+@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\\.)+[a-z]{2,}$";
     //"^[A-Za-z0-9\\u4e00-\\u9fa5]+@[a-zA-Z0-9_-]+(\\.[a-zA-Z0-9_-]+)+$"
 
-
-
-    /**
-     * jms主题订阅模式容器bean名字
-     */
-    public final static String TOPIC_CONTAINER_FACTORY = "jmsListenerContainerTopic";
-
-    /**
-     * jms队列模式容器bean名字
-     */
-    public final static String QUEUE_CONTAINER_FACTORY = "jmsListenerContainerQueue";
-
-    /**
-     * 队列名字
-     */
-    public final static String QUEUE_SCHEDULED_NAME = "scheduledQueue";
-    public final static String QUEUE_SIMPLE_NAME = "simpleQueue";
-
-    /**
-     * 主题名字
-     */
-    public final static String TOPIC_SCHEDULED_NAME = "scheduledTopic";
-    public final static String TOPIC_SIMPLE_NAME = "simpleTopic";
-
-    public final static String ROCKET_TAG_A = "r_tag_a";
-    public final static String ROCKET_TAG_B = "r_tag_b";
-    public final static String PRODUCER_INSTANCE_NAME_A = "producerInstanceA";
-    public final static String PRODUCER_INSTANCE_NAME_B = "producerInstanceB";
-    public final static String CONSUMER_INSTANCE_NAME_A = "consumerInstanceA";
-    public final static String CONSUMER_INSTANCE_NAME_B = "consumerInstanceB";
 }
